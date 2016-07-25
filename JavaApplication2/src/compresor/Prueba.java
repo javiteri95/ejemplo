cf5ed06897da101d7098db616fc70813d5e89b7f
jose viteri <jose.terivi@hotmail.com>
2016-07-25 13:44:21 -0500

    probando branches

---
+++ JavaApplication2/src/compresor/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compresor;
+
+/**
+ *
+ * @author INTEL
+ */
+public class Prueba {
+    
+    private int mundo;
+    private int posi;
+    
+    
+}
